34f9c8b1f070b8f1bdd6e95b1c75023e770e05e7
Kalori <87770235+Kalori94@users.noreply.github.com>
2024-05-20 13:26:35 -0500

    Create renameFiles_with_comprehensionList.py
    This is the second exercise to rename files using comprehension list method from coursera crash python course

---
+++ renameFiles_with_comprehensionList.py
@@ -0,0 +1,7 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate new_filenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+new_filenames = [filename.replace("hpp", "h") if filename[-3]=="hpp"else filename for filename in filenames]  # Start your code here
+
+
+print(new_filenames) 
