4f3e6b69cf006e458e71422d124cc914bcaf3364
Kalori <87770235+Kalori94@users.noreply.github.com>
2024-05-20 13:16:55 -0500

    Create renameFiles.py
    This is a basic excirse from coursera crash python course!

---
+++ renameFiles.py
@@ -0,0 +1,17 @@
+CRASH PYTHON
+RENAME FILES
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate new_filenames as a list containing the new filenames
+
+new_filenames=[]
+
+for filename in filenames:
+    if filename.endswith("hpp"):
+        new=filename.replace("hpp","h")
+        new_filenames.append(new)
+    else:
+        new_filenames.append(filename)
+
+
+print(new_filenames)
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
