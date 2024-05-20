a5c81b7a8f66812cbcc76c5059a12c4da7d6a330
Kalori <87770235+Kalori94@users.noreply.github.com>
2024-05-20 14:17:12 -0500

    Update renameFiles_with_comprehensionList.py

--- renameFiles_with_comprehensionList.py
+++ renameFiles_with_comprehensionList.py
@@ -1,7 +1,9 @@
 filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
 # Generate new_filenames as a list containing the new filenames
 # using as many lines of code as your chosen method requires.
-new_filenames = [filename.replace("hpp", "h") if filename[-3]=="hpp"else filename for filename in filenames]  # Start your code here
+new_filenames = [filename.replace("hpp", "h") if filename[-3:] == "hpp" else filename for filename in filenames]  
+
 
 
 print(new_filenames) 
+
